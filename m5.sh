ce1889457678f592401a0e57c4703791c8d11e60
robinelting <r.elting.1@student.rug.nl>
2021-03-09 16:57:54 +0100

    Updated m5.sh script

--- m5.sh
+++ m5.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Descr: Count all occurences of the article 'de' in te Dutch Wikipedia
-#		 page of the Rijksuniversiteit Groningen 
+# Descr: This shell script counts all occurences of the article 'de' in
+#        theDutch Wikipedia page of the Rijksuniversiteit Groningen
 #		 (https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen).
 #
 # Usage: ./m5.sh FILE
+# Or use: sh m5.sh FILE
+#
+# Here FILE specifies the file you would like to use. In the case of
+# the rug.txt file you should use the script as follows:
+#
+# ./m5.sh rug.txt
+# (or sh m5.sh rug.txt)
+
 
 # Check if file is specified
 FILE=$1
@@ -19,7 +27,6 @@ then
 	echo "$FILE does not exist."
 	exit
 fi
-
 
 # Output each match on a new line and ignore case distinctions,
 # count the number of lines.
